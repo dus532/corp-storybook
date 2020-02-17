55ef99a20c10398be03feb2294f877883c7e65d8
Son, Dae Seong <dssonds@humaxdigital.com>
2020-02-17 16:29:08 +0900

    [DEVOPS-71]  (1) dev-ci.dev.platdev.net (2) ssh -o 'StrictHostKeyChecking no' 옵션 사용
    Change-Id: I962dfa4156719a8e0634120abe45d1dfe84ec78c

--- script/deploy-dev.sh
+++ script/deploy-dev.sh
@@ -1,5 +1,5 @@
 chmod 600 ./ssh_key.pem
-ssh -i ./ssh_key.pem ubuntu@cisds.platdev.net \
+ssh -o 'StrictHostKeyChecking no' -i ./ssh_key.pem ubuntu@dev-ci.dev.platdev.net \
     '(cd carplat20/carplat-admin; repo sync .); \
      (chmod 600 carplat20/carplat-admin/script/ssh_key.pem); \
      (cd carplat20/carplat-infra/deploy; repo sync .; make RUN_TARGET_CLUSTER=srvsds webapp)'

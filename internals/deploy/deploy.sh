a3799e6e71282a3306630fa5105329b6cad94ca5
nyuno <nyunohyuno@gmail.com>
2020-02-19 13:52:16 +0900

    배포 환경 수정

--- internals/deploy/deploy.sh
+++ internals/deploy/deploy.sh
@@ -3,40 +3,36 @@
 RUN_TIME_ENV=$1
 BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)/build"
 
-# TODO
-#  v CDN 주소 바꿔야됨.
-#  v create invalidation에서 index.html, favicon.ico만 포함
-#  v CLI 에서 테스트로 돌린 후 확인.
-#  v ServiceWorker에 index.html, favicon.ico cache에서 제외됐는지 확인
-#  - S3 BUCKET 이름 변경 -> admin.carplat.co.kr | dev-admin.carplat.co.kr 사용중
-#  - CDN_ID 숨길지 말지 결정
-
-# Set AWS S3 bucket URL and API server environment
+# Set the S3 bucket URL and the cloudfront ID
 if [ $RUN_TIME_ENV = "development" ]
 then
-  S3_BUCKET="s3://dev-admin.carplat.co.kr"
-  CDN_ID="E1DGANF6KU25Q1"
+  S3_BUCKET="s3://admin.dev.platdev.net"
+  CDN_ID="E2FV0ED0Q8FCKY"
+
+  # Build the project
+  yarn build:dev
 elif [ $RUN_TIME_ENV = "staging" ]
 then
-  S3_BUCKET="s3://staging-admin.carplat.co.kr"
-  CDN_ID="E2FTNNK8X30JJ1"
-  touch $SERVER_ENV_FILE
+  S3_BUCKET="s3://admin.staging.platdev.net"
+  CDN_ID="E1ARZCOHX8HD18"
+
+  # Build the project
+  yarn build:stage
 elif [ $RUN_TIME_ENV = "production" ]
 then
-  S3_BUCKET="s3://admin.carplat.co.kr"
-  CDN_ID="ECM8M116GKOBP"
-  touch $SERVER_ENV_FILE
+  S3_BUCKET="s3://preprod.staging.platdev.net"
+  CDN_ID="EO9OLXQ7SE1KL"
+
+  # Build the project
+  yarn build:prod
 else
   echo "서버 환경 설정이 잘못됐습니다."
   exit
 fi
 
-# Build the project
-yarn build
-
-# Clear Bucket
+# Clear the S3 bucket
 aws s3 rm $S3_BUCKET --recursive
-# Upload the static page
+# Upload the bundle project
 aws s3 cp $BUILD_DIR $S3_BUCKET --recursive --acl public-read
-# Clear cache
+# Clear the cache
 aws cloudfront create-invalidation --distribution-id $CDN_ID --paths /index.html /favicon.ico

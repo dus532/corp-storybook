1391799a67df4c10ef045f2bb861d729e0423336
dus532 <dus532@naver.com>
2020-03-27 00:18:51 +0900

    [기타] : 인덱싱 및 소소한 수정

---
+++ app/components/index.js
@@ -0,0 +1,15 @@
+// 01 Atoms
+export { default as Button } from 'components/01Atoms/Button';
+export { default as CheckBox } from 'components/01Atoms/CheckBox';
+export { default as Logo } from 'components/01Atoms/Logo';
+
+// 02 Molecules
+export { default as Input } from 'components/02Molecules/Input';
+
+// 03 Organisms
+export { default as SignInput } from 'components/03Organisms/SignInput';
+
+// 04 Templates
+export { default as AsyncDiv } from 'components/04Templates/AsyncDiv';
+export { default as SignExtra } from 'components/04Templates/SignExtra';
+export { default as SignWrap } from 'components/04Templates/SignWrap';
--- app/pages/common/signin.js
+++ app/pages/common/signin.js
@@ -1,84 +1,34 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import LogoImg from 'images/logo.png';
-import Input from 'components/01Atoms/Input';
-import CheckBox from 'components/01Atoms/CheckBox';
-import Button from 'components/01Atoms/Button';
+import { useDispatch, useSelector } from 'react-redux';
 
-import BGImg from 'images/bg.jpg';
-
-const Logo = styled.div`
-  width: 260px;
-  height: 70px;
-  background: url(${LogoImg}) center / contain no-repeat;
-`;
-
-const Wrap = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  background: url(${BGImg}) center / cover;
-
-  h5 {
-    font-weight: 700;
-  }
-
-  .container {
-    width: 540px;
-    padding: 48px;
-    background: rgba(255, 255, 255, 0.8);
-    border: 1px solid #f2f2f2;
-    border-radius: 6px;
-  }
-
-  .flex {
-    margin-top: 4px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-right: 24px;
-    box-sizing: border-box;
-  }
-
-  .save_checkbox {
-    display: flex;
-    align-items: center;
-
-    h5 {
-      margin-left: 8px;
-    }
-  }
-`;
+import { SignWrap, SignInput, SignExtra, Button, Logo } from 'components';
+import { signHandleChange } from 'store/sign/actions';
 
 const SignIn = () => {
-  console.log(1);
+  const dispatch = useDispatch();
+  const signInData = useSelector(state => state.sign);
+
+  const handleChange = data => {
+    dispatch(signHandleChange(data.target));
+  };
+
+  const propsList = { signInData, handleChange };
 
   return (
-    <Wrap>
+    <SignWrap>
       <div className="container">
         <Logo />
         <br />
         <h5>
           프리미엄 기업 카셰어링 서비스, 카플랫 비즈니스에 오신 것을 환영합니다.
         </h5>
-        <Input placeholder="아이디" />
-        <Input type="password" placeholder="비밀번호" />
-        <div className="flex">
-          <div className="save_checkbox">
-            <CheckBox id="isSaved" />
-            <label htmlFor="isSaved">
-              <h5>로그인 상태 유지</h5>
-            </label>
-          </div>
-          <h5>아이디/비밀번호 찾기</h5>
-        </div>
+        <SignInput {...propsList} />
+        <SignExtra {...propsList} />
         <br />
         <Button>로그인</Button>
       </div>
-    </Wrap>
+    </SignWrap>
   );
 };
 

de5bfa85a889477ea8d7bb882fea730b417ce151
Son, Dae Seong <dssonds@humaxdigital.com>
2020-04-06 13:57:35 +0900

    [DEVOPS-96] biz-corp-admin work forever
    - docker --restart=always 가 default 로 설정되어 있어서
      docker container 가 종료하며 다시 restart 되고 있음
    - workaround 로 계속 돌게 설정함

--- internals/deploy/deploy.sh
+++ internals/deploy/deploy.sh
@@ -15,3 +15,9 @@ aws s3 rm $S3_BUCKET --recursive
 aws s3 cp $BUILD_DIR $S3_BUCKET --recursive --acl public-read
 # Clear the cache
 aws cloudfront create-invalidation --distribution-id $CDN_ID --paths /index.html /favicon.ico
+
+# workaround to run forever becase docker is restart=always
+while true
+do
+  sleep 1000
+done

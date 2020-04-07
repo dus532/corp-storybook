30bdd26a2b506a47346083ab4ccc0befac5dc90d
Son, Dae Seong <dssonds@humaxdigital.com>
2020-04-07 12:57:16 +0900

    [DEVOPS-96] S3 Key 를 secrets/dev/secretrc 에 둠

--- internals/deploy/deploy.sh
+++ internals/deploy/deploy.sh
@@ -1,10 +1,3 @@
-#ENV aws-access-key-id DEV_AWS_ACCESS_KEY_ID
-export AWS_ACCESS_KEY_ID=AKIAR2CPO2722REHACGK
-#ENV aws-secret-access-key DEV_AWS_SECRET_ACCESS_KEY
-export AWS_SECRET_ACCESS_KEY=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
-#ENV aws-region DEV_AWS_DEFAULT_REGION
-export AWS_DEFAULT_REGION=ap-northeast-2
-
 S3_BUCKET="s3://biz-corp-admin.dev.platdev.net"
 CDN_ID="E1SAL9QNUQW32T"
 BUILD_DIR="build"

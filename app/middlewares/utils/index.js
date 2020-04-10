98478e40057ce0120293c61f7a8b031d12c6de23
dus532 <dus532@naver.com>
2020-04-11 02:31:56 +0900

    [CPBIZ-191] Post,Put 요청시 이어지는 Get 요청을 받아오는 미들웨어 추가

--- app/middlewares/utils/index.js
+++ app/middlewares/utils/index.js
@@ -1,24 +1,33 @@
 import { KEY, LIFECYCLE } from 'middlewares/pack/constants';
-import { toggleSending } from 'store';
+import { toggleSending } from 'stores';
+
+import createActions from 'stores/controller/createActions';
 
 const utilsMiddleware = store => next => action => {
   const { type, meta } = action;
   const reqType = type.split('/')[0];
 
-  // post와 put 통신시 Sending을 위한 미들웨어!
+  // POST,PUT 통신시 Sending을 위한 미들웨어!
   switch (reqType) {
     case 'post':
     case 'put': {
       if (meta[KEY.LIFECYCLE] === LIFECYCLE.START) {
-        store.dispatch(toggleSending());
+        store.dispatch(toggleSending(true));
       } else {
-        store.dispatch(toggleSending());
+        store.dispatch(toggleSending(false));
       }
       break;
     }
     default: {
       break;
     }
+  }
+
+  // POST,PUT 이후 다시 데이터를 가져와야할 경우 사용함!
+  if (meta && meta.read && meta[KEY.LIFECYCLE] === LIFECYCLE.SUCCESS) {
+    const { read } = createActions(meta.resourceName);
+
+    store.dispatch(read({ url: meta.read }));
   }
 
   return next(action);

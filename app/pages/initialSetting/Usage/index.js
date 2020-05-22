83813167a403f948887654972b3074430f9d7755
dus532 <dus532@naver.com>
2020-05-22 23:55:47 +0900

    [CBQA-270] 초기설정에서 빠져나오게 수정

--- app/pages/initialSetting/Usage/index.js
+++ app/pages/initialSetting/Usage/index.js
@@ -14,6 +14,7 @@ import { actionPostInitialUsage } from 'stores';
 import { useToast } from 'utils/hooks';
 
 import C from 'config/constants';
+import UserManager from 'utils/userManager';
 
 const RegisterCardForm = styled.form`
   margin-top: 60px;
@@ -45,6 +46,8 @@ const Usage = () => {
   const dispatch = useDispatch();
   const toast = useToast();
 
+  const USER = UserManager().getUser();
+
   const [state, setState] = useState({
     type: 1,
     isLimited: false,
@@ -59,6 +62,7 @@ const Usage = () => {
     } else {
       dispatch(
         actionPostInitialUsage(state, () => {
+          UserManager().setUser({ ...USER, isCardRegistered: true });
           history.push('/home');
         }),
       );

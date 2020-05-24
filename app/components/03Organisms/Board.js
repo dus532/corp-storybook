ec8dff7b5537f05960ef1a4075907445f7e23230
dus532 <dus532@naver.com>
2020-05-24 17:13:23 +0900

    [CBQA-186] 공지사항 노데이터 추가

--- app/components/03Organisms/Board.js
+++ app/components/03Organisms/Board.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Color from 'config/color';
 import moment from 'utils/moment';
 import ArrowIMG from 'images/icon_arrow_black.png';
+import NoData from 'components/03Organisms/NoData';
 
 const StyledBoard = styled.div`
   background: white;
@@ -35,25 +36,28 @@ const Line = styled.div`
   }
 `;
 
-const Board = ({ now = '1', data, onClick }) => (
-  <StyledBoard className="box_overflow">
-    {data.map((d, index) => {
-      if (index >= (now * 1 - 1) * 10 && index < now * 10) {
-        return (
-          <Line key={d.id} onClick={() => onClick(d)}>
-            <div>
-              <span>{d.title}</span>
-              <br />
-              {moment(d.createdAt).format('YYYY년 MM월 DD일 HH:mm')}
-            </div>
-            <div className="arrow" />
-          </Line>
-        );
-      }
-      return null;
-    })}
-  </StyledBoard>
-);
+const Board = ({ now = '1', data, onClick }) =>
+  data.length > 0 ? (
+    <StyledBoard className="box_overflow">
+      {data.map((d, index) => {
+        if (index >= (now * 1 - 1) * 10 && index < now * 10) {
+          return (
+            <Line key={d.id} onClick={() => onClick(d)}>
+              <div>
+                <span>{d.title}</span>
+                <br />
+                {moment(d.createdAt).format('YYYY년 MM월 DD일 HH:mm')}
+              </div>
+              <div className="arrow" />
+            </Line>
+          );
+        }
+        return null;
+      })}
+    </StyledBoard>
+  ) : (
+    <NoData type="notice" />
+  );
 
 Board.propTypes = {
   now: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),

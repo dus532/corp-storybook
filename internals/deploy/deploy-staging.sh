712a781ef166708b3d0e4f5955d15999ce47bb31
Taejin Park <58160004+parktj2003-alticast@users.noreply.github.com>
2020-06-03 21:41:19 +0900

    Update deploy-staging.sh

--- internals/deploy/deploy-staging.sh
+++ internals/deploy/deploy-staging.sh
@@ -1,5 +1,18 @@
+# biz-corp-admin
 S3_BUCKET="s3://biz-corp-admin.staging.platdev.net"
 CDN_ID="E3R4V6QQQDQ8UZ"
+BUILD_DIR="build"
+
+# Clear the S3 bucket
+aws s3 rm $S3_BUCKET --recursive
+# Upload the bundle project
+aws s3 cp $BUILD_DIR $S3_BUCKET --recursive --acl public-read
+# Clear the cache
+aws cloudfront create-invalidation --distribution-id $CDN_ID --paths /index.html /favicon.ico
+
+# corp
+S3_BUCKET="s3://corp.staging.platdev.net"
+CDN_ID="EFVCEVTSM2V19"
 BUILD_DIR="build"
 
 # Clear the S3 bucket

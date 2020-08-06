cc09816474b77ecbb71a87d3b38a876416d25e2f
정연진 <dus532@naver.com>
2020-08-06 11:34:08 +0900

    [기타] build-storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,3 @@
 import { configure } from '@storybook/react';
 
-function loadStories() {
-  const req = require.context(`../app`, true, /\.stories\.jsx?$/);
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+configure(require.context('../stories', true, /\.stories\.js$/), module);
--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,6 @@ module.exports = {
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async config => {
     // do mutation to the config
-
     return config;
   },
 };

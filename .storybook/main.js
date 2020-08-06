2a295d9a442e59c45fd4ad6e93b43062efb48e93
정연진 <dus532@naver.com>
2020-08-06 16:31:51 +0900

    [기타]

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,13 @@
+const path = require('path');
+// your app's webpack.config.js
+const custom = require('../internals/webpack/webpack.prod.babel.js');
+
 module.exports = {
   stories: ['../app/stories/**/*.stories.js'],
+  webpackFinal: config => {
+    return {
+      ...config,
+      module: { ...config.module, rules: custom.module.rules },
+    };
+  },
 };
